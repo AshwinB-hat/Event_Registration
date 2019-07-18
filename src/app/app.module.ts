a1aeb65f69e6d1b31516d885cba8c737c0b9b54b
Bhat Ashwin <Ashwin.Bhat@navis.com>
2019-07-18 14:58:05 +0530

    initial commit with working structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailFormComponent } from './detail-form/detail-form.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventComponent } from './event-list/event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailFormComponent,
+    LandingComponent,
+    NavBarComponent,
+    EventListComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
